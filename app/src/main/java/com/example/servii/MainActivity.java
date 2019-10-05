99c122664da23cf6edeeaa86de508fff84958a77
Devdatt9357 <devdattchavda1692000@gmail.com>
2019-10-05 16:03:19 +0530

    working in almost all mobiles and tablets

--- app/src/main/java/com/example/servii/MainActivity.java
+++ app/src/main/java/com/example/servii/MainActivity.java
@@ -7,14 +7,13 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     private TextView google;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        google= (TextView)findViewById(R.id.main_google);
+        google=(TextView)findViewById(R.id.main_google);
         google.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
