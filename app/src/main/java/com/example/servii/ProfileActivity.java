9dd3105f70901e2062b5079b42c21489461597c2
Devdatt9357 <devdattchavda1692000@gmail.com>
2019-10-10 21:43:46 +0530

    completed profile and verify activity UI

--- app/src/main/java/com/example/servii/ProfileActivity.java
+++ app/src/main/java/com/example/servii/ProfileActivity.java
@@ -1,13 +1,29 @@
 package com.example.servii;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button cont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        cont=(Button) findViewById(R.id.profile_continue);
+        cont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this, VerifyActivity.class));
+            }
+        });
     }
 }
